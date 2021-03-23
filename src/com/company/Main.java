dbcd905815a0a9f08fcb0ba0b43be3e056726183
aimless-cpu <s.grabher@protonmail.com>
2021-03-23 14:44:41 +0100

    first commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,24 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        for (int i=0; i<1001;i++) {
+            String numberAsString = Integer.toString(i);
+            char[] numbers = numberAsString.toCharArray();
+
+            int result = 0;
+
+            for (int j=0; j< numbers.length;j++) { //wth is enhanced for?
+                result = result + Character.getNumericValue(numbers[j]);
+            }
+
+            if (result==15) {
+                System.out.println(i +" "+ result);
+            }
+        }
+
+	// write your code here
+    }
+}
