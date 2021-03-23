148b3ab69c000c1972e404864ed949f56e3cc0b6
aimless-cpu <s.grabher@protonmail.com>
2021-03-23 15:31:23 +0100

    added method

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,18 +5,7 @@ public class Main {
     public static void main(String[] args) {
 
         for (int i=0; i<1001;i++) {
-            String numberAsString = Integer.toString(i);
-            char[] numbers = numberAsString.toCharArray();
-
-            int result = 0;
-
-            /*for (int j=0; j< numbers.length;j++) { //wth is enhanced for?
-                result = result + Character.getNumericValue(numbers[j]);
-            }
-            */
-            for (char number : numbers) {
-                result = result + Character.getNumericValue(number);
-            }
+            int result = getCrossSum(i);
 
             if (result==15) {
                 System.out.println(i +" "+ result);
@@ -24,5 +13,17 @@ public class Main {
         }
 
 	// write your code here
+    }
+
+    private static int getCrossSum(int i) {
+        String numberAsString = Integer.toString(i);
+        char[] numbers = numberAsString.toCharArray();
+
+        int result = 0;
+
+        for (char number : numbers) {
+            result = result + Character.getNumericValue(number);
+        }
+        return result;
     }
 }
